c5d822a1d106e6f478152f9a55124f0296dbfe9b
maycol.rincon@sofka.com.co <maycol.rincon@sofka.com.co>
2023-10-16 15:34:04 -0500

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  public title: string = 'First App Angular';
+  public description: string = 'My cats are the coolest';
+
+
+
+
+}
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `,
+})
+export class CounterComponent {
+  private initialCounter: number = 10;
+  public counter: number = this.initialCounter;
+
+  constructor() {}
+  
+  increaseBy(value: number): void {
+    // this.counter++;
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = this.initialCounter;
+  }
+}
